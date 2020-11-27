05d6f93c2c51c6d7c0ea32ede6e068c0af2cbaa8
hulinhui <975081281@qq.com>
2020-11-27 11:15:51 +0800

    pycharm提交代码

--- AutoWeather/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Version: Python3.5
-Author: OniOn
-Site: http://www.cnblogs.com/TM0831/
-Time: 2019/2/9 16:36
-"""
--- ProxyPool/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Version: Python3.5
-Author: OniOn
-Site: http://www.cnblogs.com/TM0831/
-Time: 2019/2/12 14:53
-"""
