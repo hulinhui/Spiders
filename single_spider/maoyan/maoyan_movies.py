25a6e616c34bf8abcd461aeea005461c7ed9ea23
hulinhui <975081281@qq.com>
2020-12-11 18:52:38 +0800

    2020-12-11 网易云加密解析

--- single_spider/maoyan/maoyan_movies.py
+++ single_spider/maoyan/maoyan_movies.py
@@ -20,10 +20,24 @@ from fontTools.ttLib import TTFont
 from io import BytesIO
 
 
-headers={'User-Agent':UserAgent(verify_ssl=False).random}
+headers={'User-Agent':UserAgent(verify_ssl=False).random,'Host': 'maoyan.com',
+            'Referer':'https://maoyan.com/films?yearId=15',
+            'Sec-Fetch-Dest':'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Upgrade-Insecure-Requests': '1'}
+
+headers2={'User-Agent':UserAgent(verify_ssl=False).random,'Host': 'maoyan.com',
+            'Sec-Fetch-Dest':'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            'Upgrade-Insecure-Requests': '1'}
+
 HOST='https://maoyan.com'
 fontdict = {'uniF489': '0', 'uniE137': '8', 'uniE7A1': '9', 'uniF19B': '2', 'uniF4EF': '6',
-            'uniF848': '3', 'uniE343': '1', 'uniF53E': '4', 'uniE5E2': '5', 'uniF88A': '7',}
+            'uniF848': '3', 'uniE343': '1', 'uniE5E2': '4', 'uniE8CD': '5', 'uniF88A': '7',}
 
 
 
@@ -31,23 +45,26 @@ def get_moviescore(url):
     response=requests.get(url,headers=headers)
     if response.status_code==200:
         soup=BeautifulSoup(response.text,'lxml')
-        ddlist=soup.select('dl.movie-list > dd')
+        ddlist=soup.select('div.movies-list dd')
         for dd in ddlist:
             a=dd.select_one('a')
-            if a is not None:
-                detail_url=HOST+a.get('href')
-                get_detail_html(detail_url)
-            else:
-                print('qqq')
+            detail_url=HOST+a.get('href')
+            get_detail_html(detail_url)
     else:
         print(f'页面获取失败{url},状态码为:{response.status_code}')
 
 def get_detail_html(url):
-    response=requests.get(url,headers=headers)
+    response=requests.get(url,headers=headers2)
     if response.status_code==200:
         detail_text=response.text
         movies={}
         soup=BeautifulSoup(detail_text,'lxml')
+        data=replace_text(detail_text)          #获取评分、票房、评价等数据
+        # 获取字体文件解析[不使用，规律见fontdict]
+        # font_pattern=re.compile(",[\s\S]+?url\('(.*?)'\) format\('woff'\)")
+        # font_url='http:'+font_pattern.search(detail_text,re.S).group(1)
+        # print(font_url)
+        # get_font(font_url)
 
         #电影名称
         movies['name']=soup.select_one('div.movie-brief-container > h1').string
@@ -63,37 +80,47 @@ def get_detail_html(url):
         movies['movies_time'] = movies_country.split('/')[-1].strip()
         #上映时间
         movies['datetime']=soup.select('div.movie-brief-container > ul > li:nth-of-type(3)')[0].text
-        #获取字体文件解析[不使用，规律见fontdict]
-        # font_pattern=re.compile(",[\w\W]+?url\('(.*?)'\) format\('woff'\);")
-        # font_url='http:'+font_pattern.search(detail_text,re.S).group(1)
-        # font_dic=get_font(font_url)
         #猫眼口碑
-        data_info=re.findall('<span class="stonefont">(.*?)</span>',detail_text,re.S)
-        datalist=[]
-        for data in data_info:
-            data=data.replace('&#x','').replace(';','')
-            new_html = None
-            for key in fontdict:
-                if new_html is None:
-                    new_html = data.replace(key[3:].lower(),fontdict[key])
-                else:
-                    new_html = new_html.replace(key[3:].lower(),fontdict[key])
-            datalist.append(new_html)
-        print(datalist)
+        if len(data)<3:                     #未上映电影
+            movies['xiangkan']=data[0]
+            movies['box_office']='暂无'
+        else:                               #已上映电影
+            movies['score']=data[0]     #评分
+            movies['score_num']=data[1]+soup.select_one('span.score-num').get_text().split('万')[-1]
+            movies['box_office']=data[2]+soup.select_one('span.unit').text
+        print(movies)
     else:
         print(f'详情页获取失败{url},状态码为:{response.status_code}')
 
 
 def get_font(url):
-    resp=requests.get(url,headers=headers)
-    font=TTFont(BytesIO(resp.content))
-    font_dic=font.getBestCmap()
-    return font_dic
+    resp=requests.get(url)
+    print(resp.status_code)
+    with open('maoyanfont.woff','wb') as f:
+        f.write(resp.content)
+    font=TTFont('maoyanfont.woff')
+    font.saveXML('maoyanfont.xml')
+    font_list=font.getGlyphOrder()[2:]
+    font_list2=font.getGlyphNames()
+    print(font_list,font_list2)
 
+def replace_text(text):
+    data_info = re.findall('<span class="stonefont">(.*?)</span>', text, re.S)
+    datalist = []
+    for data in data_info:
+        data = data.replace('&#x', '').replace(';', '')
+        new_data = None
+        for key in fontdict:
+            if new_data is None:
+                new_data = data.replace(key[3:].lower(), fontdict[key])
+            else:
+                new_data = new_data.replace(key[3:].lower(), fontdict[key])
+        datalist.append(new_data)
+    return datalist
 
 
 def main():
-    url='https://maoyan.com/films'
+    url='https://maoyan.com/films?catId=3'
     get_moviescore(url)
 
 
---
+++ single_spider/wangyiyun/comments.py
@@ -0,0 +1,122 @@
+import codecs
+import requests
+import base64
+import random
+import string
+import json
+from fake_useragent import UserAgent
+from math import ceil
+from Crypto.Cipher import AES
+from multiprocessing import Pool,Lock
+
+
+
+
+class WangyiSpider():
+    def __init__(self, song_id, song_name):
+        self.song_id = song_id
+        self.song_name = song_name
+        self.headers = {"User-Agent": UserAgent().random}
+        self.url = 'https://music.163.com/weapi/comment/resource/comments/get?csrf_token='
+        # 三个固定参数
+        # bkk4o(["流泪", "强"])的值
+        self.second_param = "010001"
+        # bkk4o(YS0x.md)的值
+        self.third_param = "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7"
+        # bkk4o(["爱心", "女孩", "惊恐", "大笑"])的值
+        self.fourth_param = "0CoJUm6Qyw8W8jud"
+
+    def create_random_16(self, randomsize=16):
+        str_list = [random.choice(string.ascii_letters + string.digits) for _ in range(randomsize)]
+        random_str = ''.join(str_list)
+        return random_str
+
+    def aesEncrypt(self, a, b):
+        # 偏移量
+        iv = '0102030405060708'
+        # 文本[补齐文本长度]
+        BS = AES.block_size  # 16
+        pad = lambda s: s + (BS - len(s) % BS) * chr(BS - (len(s) % BS))
+        encryptor = AES.new(bytearray(b, 'utf-8'), AES.MODE_CBC, bytearray(iv, 'utf-8'))
+        ciphertext = encryptor.encrypt(bytearray(pad(a), 'utf-8'))
+        ciphertext = base64.b64encode(ciphertext)
+        ciphertext=ciphertext.decode('utf-8')
+        return ciphertext
+
+    def rsa_encrypt(self, i, e, f):
+        # 随机字符串逆序排列
+        text = i[::-1]
+        # 将随机字符串转换成byte类型数据
+        text = bytes(text, 'utf-8')
+        # RSA加密
+        sec_key = int(codecs.encode(text, encoding='hex'), 16) ** int(e, 16) % int(f, 16)
+        # 返回结果
+        return format(sec_key, 'x').zfill(256)
+
+    # 获取请求参数
+    def get_params(self,page=4):
+        self.frist_param = '{"csrf_token": "","cursor": "-1","offset": "0","orderType": "1","pageNo": %s,"pageSize": "20","threadId": "R_SO_4_%s"}' % (
+        page,self.song_id)
+        print(self.frist_param)
+        # 生成长度为16的随机字符串
+        text = self.create_random_16()
+        # 第一次AES加密
+        encText = self.aesEncrypt(self.frist_param, self.fourth_param)
+        # 第二次AES加密之后得到params的值
+        encText = self.aesEncrypt(encText, text)
+        # RSA加密之后得到encSecKey的值
+        encSecKey = self.rsa_encrypt(text, self.second_param, self.third_param)
+        return encText, encSecKey
+
+    # 打印评论数据
+    def get_comment(self, page):
+        params, encSecKey = self.get_params(page)
+        data = {'params': params, 'encSecKey': encSecKey}
+        resp_json = requests.post(self.url, headers=self.headers, data=data).json()
+        if resp_json and resp_json['code'] == 200:
+            print("正在爬取第{}页的评论".format(page))
+            commtent_list=resp_json['data']['comments']
+            with open(f"{self.song_name}.json",'a',encoding='utf-8') as f:
+                for data in commtent_list:
+                    item_data={}
+                    item_data['nickname']=data['user']['nickname']
+                    item_data['userId'] = data['user']['userId']
+                    item_data['avatarUrl'] = data['user']['avatarUrl']
+                    item_data['vipType'] = data['user']['vipType']
+                    item_data['content'] = data['content'].replace('\n','')
+                    print(item_data)
+                    f.write(json.dumps(item_data,ensure_ascii=False)+'\n')
+            print(f"{self.song_name}:第{page}页的评论下载成功")
+        else:
+            print(f"爬取页面{page}数据失败")
+
+    def run(self):
+        #创建进程池
+        p=Pool(processes=4)
+        # 1、获取params,encSecKey参数
+        params, encSecKey = self.get_params()
+        # 2、构造data
+        data = {'params': params, 'encSecKey': encSecKey}
+        # 请求评论接口
+        resp_json = requests.post(self.url, headers=self.headers,data=data).json()
+        # 提取评论总页数
+        if resp_json and resp_json['code'] == 200:
+            print(resp_json['data']['comments'])
+            count_page = ceil((resp_json['data']['totalCount'] - 15) / 20)
+            # page_list=[page for page in range(1,5)]
+            # p.map(self.get_comment,page_list)
+            # 获取时间戳
+            #timeteamp = resp_json['data']['cursor']
+        else:
+            print("响应数据请求失败")
+
+
+def main():
+    song_id = '1490343020'
+    song_name = '如果你也这样过'
+    wangyi = WangyiSpider(song_id, song_name)
+    wangyi.run()
+
+
+if __name__ == '__main__':
+    main()
